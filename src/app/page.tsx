c5dd7575a9da6c5afe5033de98d004d65f429f05
Asker Khuzhokov <asker.devmode@gmail.com>
2024-01-31 18:29:44 +0300

    added button to dreams block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,38 +32,45 @@ const Page = () => (
       </div>
     </section>
     <section className="relative flex w-full flex-col items-center bg-white">
-      <div className="flex w-full max-w-screen-desktop flex-col gap-10 px-2.5 pb-40 pt-20 md:flex-row md:justify-between">
-        <div className="flex max-w-[442px] flex-col gap-3">
-          <h1 className="flex flex-col gap-3 text-3xl font-bold md:text-5xl">
-            ЕСЛИ УЖ МЕЧТАТЬ,
-            <div className="flex items-center gap-3">
-              <span className="h-[5px] w-[70px] bg-black" />
-              ТО В ДЕТАЛЯХ
-            </div>
-          </h1>
-          <p className="max-w-[387px] text-base md:text-xl">
-            Делаем бесплатную 3D-визуализацию. Вам остается только выбрать
-            материалы и стиль интерьера. Либо пользоваться рекомендациями нашего
-            дизайнера.
-          </p>
+      <div className="flex w-full max-w-screen-desktop flex-col items-center px-2.5 pb-40 pt-20">
+        <div className="flex w-full flex-col items-center gap-10 md:flex-row md:justify-between">
+          <div className="flex max-w-[442px] flex-col gap-3">
+            <h1 className="flex flex-col gap-3 text-3xl font-bold md:text-5xl">
+              ЕСЛИ УЖ МЕЧТАТЬ,
+              <div className="flex items-center gap-3">
+                <span className="h-[5px] w-[70px] bg-black" />
+                ТО В ДЕТАЛЯХ
+              </div>
+            </h1>
+            <p className="max-w-[387px] text-base md:text-xl">
+              Делаем бесплатную 3D-визуализацию. Вам остается только выбрать
+              материалы и стиль интерьера. Либо пользоваться рекомендациями
+              нашего дизайнера.
+            </p>
+            <Button className="mt-4 hidden px-8 py-4 shadow-none md:flex">
+              Задать вопрос
+            </Button>
+          </div>
+
+          <div className="relative h-[150px] w-[270px] md:h-[369px] md:w-[656px]">
+            <img
+              src="/images/bush.png"
+              className="absolute right-0 top-0 z-20 h-[75px] w-[75px] -translate-y-1/3 translate-x-1/3 md:h-[180px] md:w-[180px]"
+              alt="bush"
+            />
+            <img
+              src="/images/dreams.png"
+              className="absolute left-0 top-0 z-10 h-[150px] w-[270px] md:h-[369px] md:w-[656px]"
+              alt="dreams"
+            />
+            <img
+              src="/images/bush.png"
+              className="absolute bottom-0 left-0 h-[75px] w-[75px] -translate-x-1/3 translate-y-1/3 md:h-[240px] md:w-[240px]"
+              alt="bush"
+            />
+          </div>
         </div>
-        <div className="relative h-[150px] w-[270px] md:h-[369px] md:w-[656px]">
-          <img
-            src="/images/bush.png"
-            className="absolute right-0 top-0 z-20 h-[75px] w-[75px] -translate-y-1/3 translate-x-1/3 md:h-[180px] md:w-[180px]"
-            alt="bush"
-          />
-          <img
-            src="/images/dreams.png"
-            className="absolute left-0 top-0 z-10 h-[150px] w-[270px] md:h-[369px] md:w-[656px]"
-            alt="dreams"
-          />
-          <img
-            src="/images/bush.png"
-            className="absolute bottom-0 left-0 h-[75px] w-[75px] -translate-x-1/3 translate-y-1/3 md:h-[240px] md:w-[240px]"
-            alt="bush"
-          />
-        </div>
+        <Button className="mt-12 shadow-none md:hidden">Задать вопрос</Button>
       </div>
     </section>
   </>
